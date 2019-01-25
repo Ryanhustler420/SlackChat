beda0fc40906362e2ab6c8751d7470f2e7bc3819
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-25 23:15:58 +0530

    Create Register Form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,11 +1,75 @@
 import React, {Component} from 'react';
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
+} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
 
 class Register extends Component {
+  state = {};
+
+  handleChange = () => {};
+
   render () {
     return (
-      <div>
-        Register
-      </div>
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{maxWidth: 450}}>
+          <Header as="h2" icon color="cyan" textAlign="center">
+            <Icon name="connectdevelop" color="cyan" />
+            Register for Dev's Network
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.handleChange}
+                type="email"
+              />
+
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+                type="password"
+              />
+
+              <Form.Input
+                fluid
+                name="passwordConfirmation"
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Password Confirmation"
+                onChange={this.handleChange}
+                type="password"
+              />
+
+              <Button color="cyan" fluid size="large" />
+              <Message>Already an user? <Link to="/login">Login</Link> </Message>
+            </Segment>
+          </Form>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
