8363ede9d24a96067f0bd81628e9a9cccc83eb5f
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-25 22:55:28 +0530

    Create Routes And Auth/ login.js and register.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render (<Root />, document.getElementById ('root'));
+registerServiceWorker ();
