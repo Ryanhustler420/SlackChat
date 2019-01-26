e7f212148cbb9b6a40d851f58a40861dd00bbe0d
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-26 20:13:41 +0530

    Create Messages Collection, Add First Message to Database

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -9,7 +9,7 @@ class MessagesHeader extends Component {
         <Header fluid="true" as="h2" floated="left" style={{matginBottom: 0}}>
           <span>
             Channel
-            <Icon name={'start outline'} color="black" />
+            <Icon name='star outline' color="black" />
           </span>
           <Header.Subheader>2 Users</Header.Subheader>
         </Header>
