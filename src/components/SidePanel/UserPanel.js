92e29fd3afd1714a9877a38be4109f0f2f474046
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-26 10:45:01 +0530

    App Header  UserPanel Dropdown

---
+++ src/components/SidePanel/UserPanel.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import {Grid, Header, Icon, Label, Dropdown} from 'semantic-ui-react';
+
+class UserPanel extends Component {
+  dropDownOptions = () => [
+    {
+      key: 'user',
+      text: <span>Signed in as <strong>User</strong></span>,
+      disabled: true,
+    },
+    {
+      key: 'avatar',
+      text: <span>Change Avatag</span>,
+    },
+    {
+      key: 'signout',
+      text: <span>Sign Out</span>,
+    },
+  ];
+
+  render () {
+    return (
+      <Grid style={{background: '#4c3c5c'}}>
+        <Grid.Column>
+          <Grid.Row style={{padding: '1.2em', margin: 0}}>
+            {/* App Header */}
+            <Header inverted floated="left" as="h2">
+              <Icon name="code" />
+              <Header.Content>
+                Dev's Network
+              </Header.Content>
+              <Label as="a" image>
+                <img src="https://avatars2.githubusercontent.com/u/25275856?s=460&v=4" />
+                <Icon name="superpowers" />Gaurav Gupta
+              </Label>
+              {/* <Label>
+                Creator
+              </Label> */}
+            </Header>
+          </Grid.Row>
+          {/* User Dropdown */}
+          <Header style={{padding: '0.25em'}} as="h4" inverted>
+            <Dropdown
+              trigger={<span>User</span>}
+              options={this.dropDownOptions ()}
+            />
+          </Header>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default UserPanel;
