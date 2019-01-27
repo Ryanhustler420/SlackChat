e2ab0800bdde5fb50515bdccd33524a052b06920
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-27 16:08:34 +0530

    Add Users to DirectMessages Component, Show If OnlineOffline

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,10 +1,72 @@
 import React, {Component} from 'react';
 import {Menu, Icon} from 'semantic-ui-react';
+import firebase from './../../firebase';
 
 class DirectMessages extends Component {
   state = {
+    user: this.props.currentUser,
     users: [],
+    usersRef: firebase.database ().ref ('users'),
+    connectedRef: firebase.database ().ref ('.info/connected'),
+    presenceRef: firebase.database ().ref ('presence'),
   };
+
+  componentDidMount () {
+    if (this.state.user) {
+      this.addListeners (this.state.user.uid);
+    }
+  }
+
+  addListeners = UserUId => {
+    let loadedUsers = [];
+    this.state.usersRef.on ('child_added', snap => {
+      if (UserUId !== snap.key) {
+        let user = snap.val ();
+        user['uid'] = snap.key;
+        user['status'] = 'offline';
+        loadedUsers.push (user);
+        this.setState ({users: loadedUsers});
+      }
+    });
+
+    this.state.connectedRef.on ('value', snap => {
+      if (snap.val () === true) {
+        const ref = this.state.presenceRef.child (UserUId);
+        ref.set (true);
+        ref.onDisconnect ().remove (err => {
+          if (err !== null) {
+            console.error (err);
+          }
+        });
+      }
+    });
+
+    this.state.presenceRef.on ('child_added', snap => {
+      if (UserUId !== snap.key) {
+        // add status to user
+        this.addStatusToUSer (snap.key);
+      }
+    });
+
+    this.state.presenceRef.on ('child_removed', snap => {
+      if (UserUId !== snap.key) {
+        // remove status to user
+        this.addStatusToUSer (snap.key, false);
+      }
+    });
+  };
+
+  addStatusToUSer = (user_id, connected = true) => {
+    const updatedUsers = this.state.users.reduce ((acc, user) => {
+      if (user.uid === user_id) {
+        user['status'] = `${connected ? 'online' : 'offline'}`;
+      }
+      return acc.concat (user);
+    }, []);
+    this.setState ({users: updatedUsers});
+  };
+
+  isUserOnline = user => user.status === 'online';
 
   render () {
     const {users} = this.state;
@@ -17,6 +79,19 @@ class DirectMessages extends Component {
           ({users.length})
         </Menu.Item>
         {/* users to Send Direct Messages */}
+        {users.map (user => (
+          <Menu.Item
+            key={user.uid}
+            onClick={() => console.log}
+            style={{opacity: 0.7, fontStyle: 'italic'}}
+          >
+            <Icon
+              name="circle"
+              color={this.isUserOnline (user) ? 'green' : 'red'}
+            />
+            @ {user.name}
+          </Menu.Item>
+        ))}
       </Menu.Menu>
     );
   }
